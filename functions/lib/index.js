061c52db148e1f873ebe26d5a26df043b3453e78
jirivrba <jirivrba@users.noreply.github.com>
2026-01-01 23:39:52 +0100

    Update functions

--- functions/lib/index.js
+++ functions/lib/index.js
@@ -2797,8 +2797,10 @@ exports.sendProfileChangeEmail = functions
         let hasOtherChanges = false;
         const otherChanges = [];
         for (const key of allKeys) {
+            // Ignorovat všechny ignorovaná pole (včetně updatedAt)
             if (ignoredFields.includes(key))
                 continue;
+            // Ignorovat foto-related pole
             if (photoRelatedFields.includes(key))
                 continue;
             const oldVal = beforeData[key];
@@ -2809,7 +2811,7 @@ exports.sendProfileChangeEmail = functions
             if (oldVal && typeof oldVal === 'object' && 'toDate' in oldVal) {
                 oldNormalized = oldVal.toDate().getTime();
             }
-            else if (oldVal === null || oldVal === undefined) {
+            else if (oldVal === null || oldVal === undefined || oldVal === "") {
                 oldNormalized = "";
             }
             else {
@@ -2818,7 +2820,7 @@ exports.sendProfileChangeEmail = functions
             if (newVal && typeof newVal === 'object' && 'toDate' in newVal) {
                 newNormalized = newVal.toDate().getTime();
             }
-            else if (newVal === null || newVal === undefined) {
+            else if (newVal === null || newVal === undefined || newVal === "") {
                 newNormalized = "";
             }
             else {
@@ -2830,13 +2832,22 @@ exports.sendProfileChangeEmail = functions
                 break; // Stačí najít jednu změnu
             }
         }
-        // Pokud se mění pouze foto-related pole, neposílat email
+        // Pokud se mění pouze foto-related pole (a možná updatedAt, který je ignorován), neposílat email
         if (!hasOtherChanges) {
             functions.logger.info("Změna pouze profilové fotky, email se neposílá", {
                 userId,
                 photoChanges,
                 beforeKeys: Object.keys(beforeData),
-                afterKeys: Object.keys(afterData)
+                afterKeys: Object.keys(afterData),
+                allChangedKeys: Array.from(allKeys).filter(k => {
+                    const oldVal = beforeData[k];
+                    const newVal = afterData[k];
+                    if (oldVal === undefined && newVal === undefined)
+                        return false;
+                    if (oldVal === undefined || newVal === undefined)
+                        return true;
+                    return JSON.stringify(oldVal) !== JSON.stringify(newVal);
+                })
             });
             return null;
         }
